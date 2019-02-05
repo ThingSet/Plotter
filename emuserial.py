88532fa17c38db5304d43f0dc313c8af7d289d1c
Kathrin <weihek@gmail.com>
2019-02-05 13:59:03 +0100

    added sqlite_worker.py: Writes data from serial port to data/solarbox.db

--- emuserial.py
+++ emuserial.py
@@ -1,19 +1,32 @@
-import serial
+#!/usr/bin/env python
+""" This script parses raw ThingSet data from a file
+and sends it to a given serial port every 1 second."""
+
 import argparse
+import time
+import serial
 
-parser = argparse.ArgumentParser()
-parser.add_argument("port", help="Serial port to stream data to")
-args = parser.parse_args()
+def main():
+    """Open serial port, read lines from raw_data.txt and send it to serial port."""
+    parser = argparse.ArgumentParser()
+    parser.add_argument("port", help="Serial port to stream data to")
+    args = parser.parse_args()
 
-portName = args.port
+    port_name = args.port
 
-with open('data/raw_data.txt') as f:
-    raw_data = f.readlines()
+    with open('data/raw_data.txt') as f:
+        raw_data = f.readlines()
 
-ser = serial.Serial()
-ser.baudrate = 57600
-ser.port = portName
-ser.open()
+    ser = serial.Serial()
+    ser.baudrate = 57600
+    ser.port = port_name
+    ser.open()
 
-for line in raw_data:
-    ser.write(line.encode('utf-8'))
+    for line in raw_data:
+        ser.write(line.encode('utf-8'))
+        print("Sent line.")
+        time.sleep(1)
+
+
+if __name__ == '__main__':
+    main()
