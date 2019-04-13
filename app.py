0b271b3bb8b5749b6fb1602350b668c2908e99b0
Martin Jaeger <17674105+martinjaeger@users.noreply.github.com>
2019-04-13 16:13:10 +0200

    Added tabs and second graph

--- app.py
+++ app.py
@@ -23,33 +23,57 @@ meas.remove('index')
 opt = [dict(label=item, value=item) for item in meas]
 
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
+app.config['suppress_callback_exceptions']=True
 
 app.layout = html.Div(children=[
-    html.H1(children='LibreSolar Data Visualization'),
-    html.Div('Choose a value to display'),
-    dcc.Dropdown(
-        id='meas-dropdown',
-        options=opt,
-        multi=True
-    ),
-    dcc.Graph(id='solarbox'),
-    html.Div([
-        daq.BooleanSwitch(
-            id='my-daq-booleanswitch',
-            label='Send boolean value',
-            on=True
-        )],
-        style={'padding': '20px 0px 20px 0px'}
-    ),
-    html.Div(id='output-boolean', style={'align':'center'}),
-    daq.NumericInput(
-        id='my-numeric-input',
-        label='Set numeric value',
-        value=0,
-        style={'padding': '20px 0px 20px 0px'}
-    ),
-    html.Div(id='numeric-input-output')
+    html.H1(children='Libre Solar ThingSet Interface'),
+    dcc.Tabs(id="tabs-example", value='tab-data', children=[
+        dcc.Tab(label='Data', value='tab-data'),
+        dcc.Tab(label='Configuration', value='tab-conf'),
+    ]),
+    html.Div(id='tabs-content')
 ], className="container")
+
+
+@app.callback(Output('tabs-content', 'children'),
+              [Input('tabs-example', 'value')])
+def render_content(tab):
+    if tab == 'tab-data':
+        return html.Div([
+            dcc.Dropdown(
+                id='data-sel-1',
+                options=opt,
+                multi=True
+            ),
+            dcc.Graph(id='graph-1'),
+
+            dcc.Dropdown(
+                id='data-sel-2',
+                options=opt,
+                multi=True
+            ),
+            dcc.Graph(id='graph-2'),
+        ])
+    elif tab == 'tab-conf':
+        return html.Div([
+            html.Div([
+                daq.BooleanSwitch(
+                    id='my-daq-booleanswitch',
+                    label='Send boolean value',
+                    on=True
+                )],
+                style={'padding': '20px 0px 20px 0px'}
+            ),
+            html.Div(id='output-boolean', style={'align':'center'}),
+            daq.NumericInput(
+                id='my-numeric-input',
+                label='Set numeric value',
+                value=0,
+                style={'padding': '20px 0px 20px 0px'}
+            ),
+            html.Div(id='numeric-input-output')
+        ])
+
 
 @app.callback(
     dash.dependencies.Output('output-boolean', 'children'),
@@ -64,16 +88,43 @@ def update_output_switch(on):
 def update_output(value):
     return 'The value is {}.'.format(value)
 
-@app.callback(Output('solarbox', 'figure'),
-              [Input('meas-dropdown', 'value')])
+@app.callback(Output('graph-1', 'figure'),
+              [Input('data-sel-1', 'value')])
 def update_graph(selected_dropdown_value):
     if not selected_dropdown_value:
         pass
     else:
         return {
             'data': [{
-                'x': df.index,
+                'x': df.time,
                 'y': df[value],
+                'name': value,
+                'line': {
+                    'width': 3,
+                    'shape': 'spline'
+                }
+            } for value in selected_dropdown_value],
+            'layout': {
+                'margin': {
+                    'l': 100,
+                    'r': 100,
+                    'b': 30,
+                    't': 30
+                }
+            }
+        }
+
+@app.callback(Output('graph-2', 'figure'),
+              [Input('data-sel-2', 'value')])
+def update_graph(selected_dropdown_value):
+    if not selected_dropdown_value:
+        pass
+    else:
+        return {
+            'data': [{
+                'x': df.time,
+                'y': df[value],
+                'name': value,
                 'line': {
                     'width': 3,
                     'shape': 'spline'
